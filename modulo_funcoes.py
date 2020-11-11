b2b2e35926e55fedef5651f9fc4f7ddafd83f5c8
TeresaCoimbra pg42883 <55380903+TeresaCoimbra@users.noreply.github.com>
2020-11-11 15:30:53 +0000

    Update modulo_funcoes.py

--- modulo_funcoes.py
+++ modulo_funcoes.py
@@ -105,15 +105,36 @@ def traducao(seq):
 
     Parameters
     ----------
-    seq : TYPE
-        DESCRIPTION.
+    seq : str
+        sequência de DNA
 
     Returns
     -------
-    None.
+        Tradução de uma sequência de DNA.
 
     """
-    pass
+    seq = seq.upper()
+    codoes = re.findall('...',seq)
+    aas = ''
+    gencode = {'ATA':'I', 'ATC':'I', 'ATT':'I', 
+    'ATG':'M', 'ACA':'T', 'ACC':'T', 'ACG':'T', 
+    'ACT':'T','AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+    for i in codoes:
+        aas += gencode[i]
+    return(aas)
 
 def valida(seq):
     """ Funcao que verifica se a sequencia de DNA e valida (devolve True or False)
